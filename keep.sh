adb3d2c1fe3d0639f1aa3e6a7841d397b18d8215
kanezikii <peng320829@gmail.com>
2026-03-19 11:24:14 +0800

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 环境变量配置
-EMAIL="xxxxx@xxxx.com"   # 登录邮箱
-PASSWORD="xxxxxxxxx"     # 登录密码
+EMAIL="peng320829@gmail.com"   # 登录邮箱
+PASSWORD="Qwer12138."     # 登录密码
 
 # 要保活的URL列表,多个用英文空格分隔
-URLS="https://xxxx.cfapps.ap21.hana.ondemand.com https://xxxx.cfapps.us10-001.hana.ondemand.com"  
+URLS="https://free-ussrmbrw.cfapps.us10-001.hana.ondemand.com https://free-sguslnkx.cfapps.ap21.hana.ondemand.com"  
 
 # 颜色定义
 green() {
